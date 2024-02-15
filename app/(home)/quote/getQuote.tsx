25745ef8c506166c25c522073d0ab5d42fd6cb5a
Shubham Rawat <Sr856161@gmail.com>
2024-02-15 14:25:38 +0530

    quote set to today

--- app/(home)/quote/getQuote.tsx
+++ app/(home)/quote/getQuote.tsx
@@ -1,7 +1,7 @@
 export const revalidate = 600; // 10 min
 
 export default async function getQuote() {
-  const res = await fetch("https://zenquotes.io/api/random");
+  const res = await fetch("https://zenquotes.io/api/today");
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
